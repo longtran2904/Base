17322f39b25c8f404db465c3452778f8e78caecb
longtran2904 <53656110+longtran2904@users.noreply.github.com>
2025-10-16 16:13:35 +0700

    Experimenting with LindBound to handle overlapping lines at each block boundary

--- bench_disk.bat
+++ bench_disk.bat
@@ -1,45 +1,45 @@
 @echo off
-for /L %%i in (1,1,5) do (
+for /L %%i in (1,1,3) do (
     D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:32 foo & timeout /t 5 > NUL
-    D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:32 foo ..\Handmade-Hero & timeout /t 5 > NUL
+    D:\Programs\RAMMap -Et & timeout /t 5 > NUL &   glob_seq -r -d -f:32 foo & timeout /t 5 > NUL
 )
 
-for /L %%i in (1,1,5) do (
+for /L %%i in (1,1,3) do (
     D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:64 foo & timeout /t 5 > NUL
-    D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:64 foo ..\Handmade-Hero & timeout /t 5 > NUL
+    D:\Programs\RAMMap -Et & timeout /t 5 > NUL &   glob_seq -r -d -f:64 foo & timeout /t 5 > NUL
 )
 
-for /L %%i in (1,1,5) do (
+for /L %%i in (1,1,3) do (
     D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:128 foo & timeout /t 5 > NUL
-    D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:128 foo ..\Handmade-Hero & timeout /t 5 > NUL
+    D:\Programs\RAMMap -Et & timeout /t 5 > NUL &   glob_seq -r -d -f:128 foo & timeout /t 5 > NUL
 )
 
-for /L %%i in (1,1,5) do (
+for /L %%i in (1,1,3) do (
     D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:256 foo & timeout /t 5 > NUL
-    D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:256 foo ..\Handmade-Hero & timeout /t 5 > NUL
+    D:\Programs\RAMMap -Et & timeout /t 5 > NUL &   glob_seq -r -d -f:256 foo & timeout /t 5 > NUL
 )
 
-for /L %%i in (1,1,5) do (
+for /L %%i in (1,1,3) do (
     D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:512 foo & timeout /t 5 > NUL
-    D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:512 foo ..\Handmade-Hero & timeout /t 5 > NUL
+    D:\Programs\RAMMap -Et & timeout /t 5 > NUL &   glob_seq -r -d -f:512 foo & timeout /t 5 > NUL
 )
 
-for /L %%i in (1,1,5) do (
+for /L %%i in (1,1,3) do (
     D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:1024 foo & timeout /t 5 > NUL
-    D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:1024 foo ..\Handmade-Hero & timeout /t 5 > NUL
+    D:\Programs\RAMMap -Et & timeout /t 5 > NUL &   glob_seq -r -d -f:1024 foo & timeout /t 5 > NUL
 )
 
-for /L %%i in (1,1,5) do (
+for /L %%i in (1,1,3) do (
     D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:2056 foo & timeout /t 5 > NUL
-    D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:2056 foo ..\Handmade-Hero & timeout /t 5 > NUL
+    D:\Programs\RAMMap -Et & timeout /t 5 > NUL &   glob_seq -r -d -f:2056 foo & timeout /t 5 > NUL
 )
 
-for /L %%i in (1,1,5) do (
+for /L %%i in (1,1,3) do (
     D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:4096 foo & timeout /t 5 > NUL
-    D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:4096 foo ..\Handmade-Hero & timeout /t 5 > NUL
+    D:\Programs\RAMMap -Et & timeout /t 5 > NUL &   glob_seq -r -d -f:4096 foo & timeout /t 5 > NUL
 )
 
-for /L %%i in (1,1,5) do (
+for /L %%i in (1,1,3) do (
     D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:8192 foo & timeout /t 5 > NUL
-    D:\Programs\RAMMap -Et & timeout /t 5 > NUL & build\glob -r -d -f:8192 foo ..\Handmade-Hero & timeout /t 5 > NUL
+    D:\Programs\RAMMap -Et & timeout /t 5 > NUL &   glob_seq -r -d -f:8192 foo & timeout /t 5 > NUL
 )
--- code/examples/glob.c
+++ code/examples/glob.c
@@ -3,7 +3,7 @@
 #endif
 
 #if !GLOB_STATIC_LIB
-#define ENABLE_ASSERT 0
+#define ENABLE_ASSERT 1
 #endif
 
 #include "Base.h"
@@ -214,10 +214,15 @@ enum
 
 global String userPattern;
 
-function u64 GlobFile(String text, String file)
+function u64 GlobFile(String text, String file, u64* lineCount)
 {
     u64 matchCount = 0;
-    for (i64 index, row = 1; text.size; text = StrSkip(text, index + 1), ++row)
+    Assert(text.size);
+    if (text.str[text.size - 1] == '\r')
+        text.size--;
+    
+    u64 row = *lineCount + 1;
+    for (i64 index; text.size; text = StrSkip(text, index + 1), ++row)
     {
         index = StrFindChr(text, "\n");
         
@@ -237,16 +242,18 @@ function u64 GlobFile(String text, String file)
         }
     }
     
+    *lineCount = row;
     return matchCount;
 }
 
-// NOTE(long): Async reads are ~1.5x faster on my HDD drive, while ~3-5x faster on my SSD drive (~3GB tests)
-
 global OS_Handle iocp;
-global u64 pushFileMs;
 global u64 fileCount;
 global u64 totalSize;
 global u64 memoryUsage;
+
+#define MAX_REQUESTS 32
+global volatile u64 requestCount;
+global u64 chunkSize = KiB(64);
 
 #include <stdlib.h>
 
@@ -258,188 +265,21 @@ struct ThreadResult
     u64 matchCount;
 };
 
-#define CHUNK_SIZE KiB(64)
-#define MAX_REQUESTS 32
-
-//- DiskMark
-// 1024: 59.09 61.23 61.47 61.43 61.56
-//  512: 58.83 59.04 59.58 59.93 59.87
-//  256: 60.68 60.87 60.97 60.80 60.82
-//  128: 59.49 60.69 60.87 60.93 60.28
-//   64: 59.08 58.75 59.53 59.28 58.01
-
-//- CHUNKING BUFFER
-
-// 32 KiB
-//c (55.31 + 56.15 + 55.07 + 55.51 + 55.76) / 5
-//c (114.95 + 115.89 + 115.89 + 116.20 + 115.51) / 5
-
-// 64 KiB
-//c (58.40 + 59.06 + 58.06 + 58.78 + 58.31) / 5
-//c (116.39 + 117.25 + 116.36 + 117.13 + 115.76) / 5
-
-// 128 KiB
-//c (58.47 + 58.58 + 58.38 + 59.01 + 59.10) / 5
-//c (116.20 + 116.45 + 115.98 + 116.80 + 116.52) / 5
-
-// 256 KiB
-//c (59.00 + 58.89 + 58.62 + 58.38 + 58.56) / 5
-//c (116.71 + 116.93 + 117.90 + 116.33 + 117.70) / 5
-
-// 512 KiB
-//c (55.80 + 56.24 + 55.72 + 56.19 + 56.02) / 5
-//c (116.14 + 116.84 + 116.33 + 116.23 + 115.39) / 5
-
-// 1024 KiB
-//c (56.37 + 56.88 + 57.16 + 56.19 + 56.68) / 5
-//c (116.61 + 116.23 + 116.04 + 116.39 + 116.74) / 5
-
-// 2048 KiB
-//c (57.21 + 57.30 + 56.88 + 57.23 + 57.05) / 5
-//c (117.16 + 116.91 + 116.55 + 116.49 + 117.35) / 5
-
-// 4096 KiB
-//c (57.63 + 57.57 + 57.78 + 57.44 + 57.76) / 5
-//c (116.14 + 117.22 + 116.90 + 116.68 + 117.38) / 5
-
-// 8192 KiB
-//c (57.44 + 57.96 + 57.49 + 57.33 + 57.90) / 5
-//c (116.74 + 116.11 + 116.52 + 117.83 + 116.67) / 5
-
-//- OUTSTANDING BUFFER
-
-// 32 KiB
-//c (57.64 + 58.65 + 58.06 + 58.46 + 57.90) / 5
-//c (117.15 + 117.93 + 117.03 + 117.00 + 116.83) / 5
-
-// 64 KiB
-//c (57.74 + 57.28 + 57.20 + 57.61 + 57.66) / 5
-//c (116.81 + 116.39 + 116.93 + 116.93 + 116.90) / 5
-
-// 128 KiB
-//c (58.07 + 54.46 + 58.38 + 58.15 + 57.65) / 5
-//c (117.73 + 117.64 + 116.97 + 117.64 + 116.80) / 5
-
-// 256 KiB
-//c (57.73 + 57.83 + 57.46 + 57.43 + 57.82) / 5
-//c (117.48 + 117.51 + 116.72 + 117.54 + 117.19) / 5
-
-// 512 KiB
-//c (58.00 + 57.56 + 57.50 + 57.63 + 57.95) / 5
-//c (117.45 + 117.48 + 116.71 + 116.77 + 117.28) / 5
-
-// 1024 KiB
-//c (58.23 + 58.00 + 57.79 + 57.07 + 57.73) / 5
-//c (115.29 + 116.55 + 115.82 + 115.17 + 114.95) / 5
-
-// 2048 KiB
-//c (56.49 + 57.94 + 57.58 + 57.80 + 57.45) / 5
-//c (112.59 + 113.04 + 112.86 + 112.66 + 113.49) / 5
-
-#if !CHUNK_SIZE
-typedef struct FileEntry FileEntry;
-struct FileEntry
+typedef struct LineBound LineBound;
+struct LineBound
 {
-    String path;
-    String data;
-    OS_Handle file;
+    LineBound* next;
+    u64 offset;
+    String prefix;
+    String suffix;
 };
 
-// TODO(long): https://youtu.be/xbvaeBCbDDA?t=498
-// workSize is only used for 2 things:
-// 1. Signal that all works are done
-// 2. Keep track of the maximum allocation size
-// The first one can be replaced with a single bool where one thread read and the other thread write
-// The second one can be replaced with each thread having a separate int
-global volatile u64 workSize;
-global u64 desiredSize = MiB(512);
-
-function void ThreadProc(void* arg)
-{
-    volatile ThreadResult* info = arg;
-    
-    while (1)
-    {
-        FileEntry* entry = 0;
-        TIME_BLOCK(duration, info->waitMs += duration)
-        {
-            u64 bytesRead = OS_FileWaitAsync(iocp, &entry, INFINITE);
-            if (bytesRead == 0)
-                Errf("ERROR: Failed to wait for file\n");
-        }
-        
-        if (ALWAYS(entry))
-        {
-#if 0
-            TIME_BLOCK(duration, info->workMs += duration)
-                info->matchCount += GlobFile(entry->data, entry->path);
-#endif
-            
-            OS_FileClose(entry->file);
-            _aligned_free(entry->data.str);
-            AtomicSub64(&workSize, entry->data.size);
-        }
-    }
-}
-
-function u64 ReadFilesAsync(Arena* arena, StringList* paths)
-{
-    u64 maxSize = 0;
-    u64 loadMs = OSNowMS();
-    
-    StrListIter(paths, pathNode)
-    {
-        String path = pathNode->string;
-        
-        TIME_BLOCK(duration, pushFileMs += duration)
-        {
-            OS_Handle file = OS_FileOpen(path, AccessFlag_Read|AccessFlag_NoCache, iocp);
-            u64 size = OS_FileProp(file).size;
-            
-            if (size)
-            {
-                FileEntry* entry = PushStruct(arena, FileEntry);
-                entry->path = path;
-                entry->file = file;
-                entry->data.size = size;
-                
-                void* buffer = entry->data.str = _aligned_malloc(AlignUpPow2(size, 4096), 4096);
-                if (!buffer)
-                    Errf("ERROR: \"%.*s\" is too big (%.2f MiB)\n", StrExpand(path), DivF64(size, MiB(1)));
-                else if (!OS_FileReadAsync(file, (r1u64){0}, buffer, entry))
-                    Errf("ERROR: \"%.*s\"\n", StrExpand(path));
-                
-                else
-                {
-                    fileCount++;
-                    totalSize += size;
-                    
-                    size = AtomicAdd64(&workSize, size);
-                    maxSize = Max(maxSize, size);
-                }
-            }
-        }
-        
-        while (workSize >= desiredSize)
-            _mm_pause();
-    }
-    
-    while (workSize)
-        _mm_pause();
-    
-    loadMs = OSNowMS() - loadMs;
-    Outf("Bandwidth: %.2f MiB/s, Loaded Files: %llu (%.2f MiB)\n",
-         DivF64(totalSize, 1000 * loadMs), fileCount, DivF64(totalSize, MiB(1)));
-    return maxSize;
-}
-
-#else
 typedef struct FileState FileState;
 struct FileState
 {
     Arena* arena;
     String path;
-    StringList lines;
+    LineBound* bounds;
     OS_Handle file;
     u64 size;
     volatile u64 readCount;
@@ -450,11 +290,26 @@ struct FileEntry
 {
     FileState* state;
     String data;
+    String path;
     u64 offset;
 };
 
-global volatile u64 requestCount;
-global u64 chunkSize = CHUNK_SIZE;
+function LineBound* LookupBound(FileState* state, u64 offset, b32* found)
+{
+    for (LineBound* bound = state->bounds; bound; bound = bound->next)
+    {
+        if (bound->offset == offset)
+        {
+            *found = 1;
+            return bound;
+        }
+    }
+    
+    LineBound* node = PushStruct(state->arena, LineBound);
+    node->offset = offset;
+    SLLStackPush(state->bounds, node);
+    return node;
+}
 
 function void ThreadProc(void* arg)
 {
@@ -473,33 +328,73 @@ function void ThreadProc(void* arg)
         if (ALWAYS(entry))
         {
             FileState* state = entry->state;
-            if (AtomicDec64(&state->readCount) == 0)
-                OS_FileClose(state->file);
-            AtomicDec64(&requestCount);
-            
-#if 0
             TIME_BLOCK(duration, info->workMs += duration)
             {
                 String data = entry->data;
-                if (entry->offset + chunkSize < state->size)
+                
+                if (entry->offset > 0)
                 {
-                    i64 lastLine = StrFindArr(data, StrLit("\r\n"), MatchStr_LastMatch);
+                    i64 firstLine = StrFindChr(data, "\n");
+                    if (firstLine > 0)
+                    {
+                        b32 found = 0;
+                        LineBound* bound = LookupBound(state, entry->offset, &found);
+                        
+                        if (found)
+                        {
+                            data = StrJoin3(state->arena, bound->prefix, data);
+                            // TODO(long): free bound
+                        }
+                        else
+                        {
+                            bound->suffix = StrCopy(state->arena, StrChop(data, firstLine));
+                            data = StrSkip(data, firstLine + 1);
+                        }
+                    }
+                }
+                
+                u64 nextOffset = entry->offset + chunkSize;
+                if (nextOffset < state->size)
+                {
+                    i64 lastLine = StrFindArr(data, StrLit("\n"), MatchStr_LastMatch);
                     if (lastLine != data.size - 1)
                     {
-#if 0
-                        String leftover = StrCopy(state->arena, StrSkip(data, lastLine + 1));
-                        StrListPush(state->arena, &state->lines, leftover);
-#endif
+                        b32 found = 0;
+                        LineBound* bound = LookupBound(state, nextOffset, &found);
                         
-                        data = StrPrefix(data, lastLine);
+                        if (found)
+                        {
+                            data = StrJoin3(state->arena, data, bound->suffix);
+                            // TODO(long): free bound
+                        }
+                        else
+                        {
+                            bound->prefix = StrCopy(state->arena, StrSkip(data, lastLine + 1));
+                            data = StrPrefix(data, lastLine);
+                        }
                     }
                 }
                 
                 if (data.size)
-                    info->matchCount += GlobFile(data, state->path);
+                    info->matchCount += GlobFile(data, state->path, &(u64){0});
             }
-#endif
             
+            // NOTE(long): Pop after GlobFile to ensure the program
+            // exits only after all files have been fully globbed
+            if (AtomicDec64(&state->readCount) == 0)
+            {
+                for (LineBound* bound = state->bounds; bound; bound = bound->next)
+                {
+                    String line = StrJoin3(state->arena, bound->prefix, bound->suffix);
+                    if (line.size)
+                        info->matchCount += GlobFile(line, state->path, &(u64){0});
+                }
+                
+                OS_FileClose(state->file);
+                ArenaRelease(state->arena);
+            }
+            
+            AtomicDec64(&requestCount);
             _aligned_free(entry->data.str);
         }
     }
@@ -508,63 +403,61 @@ function void ThreadProc(void* arg)
 function u64 ReadFilesAsync(Arena* arena, StringList* paths)
 {
     u64 maxSize = 0;
-    u64 stallMs = 0;
     u64  loadMs = OSNowMS();
     
     StrListIter(paths, pathNode)
     {
-        TIME_BLOCK(duration, pushFileMs += duration)
+        String path = pathNode->string;
+        OS_Handle file = OS_FileOpen(path, AccessFlag_Read|AccessFlag_NoCache, iocp);
+        u64 size = OS_FileProp(file).size;
+        
+        FileState* state = PushStruct(arena, FileState);
+        state->arena = ArenaMake(1);
+        state->path = path;
+        state->file = file;
+        state->size = size;
+        
+        u64 readCount = (u64)Ceil_f32(DivF32(size, chunkSize));
+        state->readCount = readCount;
+        
+        for (u64 offset = 0; offset < size; offset += chunkSize)
         {
-            String path = pathNode->string;
-            OS_Handle file = OS_FileOpen(path, AccessFlag_Read|AccessFlag_NoCache, iocp);
-            u64 size = OS_FileProp(file).size;
+            // NOTE(long): Can't use state->arena because ThreadProc might still be using it; not thread-safe
+            FileEntry* entry = PushStruct(arena, FileEntry);
+            entry->path = path;
+            entry->state = state;
+            entry->offset = offset;
             
-            FileState* state = PushStruct(arena, FileState);
-            state->arena = ArenaMake();
-            state->path = path;
-            state->file = file;
-            state->size = size;
-            state->readCount = (u64)Ceil_f32((f32)size / chunkSize);
+            entry->data.size = Min(size - entry->offset, chunkSize);
+            u64 entrySize = AlignUpPow2(entry->data.size, 4096);
+            entry->data.str = _aligned_malloc(entrySize, 4096);
             
-            for (u64 offset = 0; offset < size; offset += chunkSize)
+            if (ALWAYS(OS_FileReadAsync(file, R1U64Size(entry->offset, entrySize), entry->data.str, entry)))
             {
-                FileEntry* entry = PushStruct(arena, FileEntry);
-                entry->state = state;
-                entry->offset = offset;
-                
-                entry->data.size = Min(size - entry->offset, chunkSize);
-                u64 entrySize = AlignUpPow2(entry->data.size, 4096);
-                entry->data.str = _aligned_malloc(entrySize, 4096);
-                
-                if (OS_FileReadAsync(file, R1U64Size(entry->offset, entrySize), entry->data.str, entry))
+                if (offset == 0)
                 {
-                    if (offset == 0)
-                    {
-                        fileCount++;
-                        totalSize += size;
-                        maxSize = Max(size, maxSize);
-                    }
-                    
-                    AtomicInc64(&requestCount);
-                    TIME_BLOCK(stallDuration, stallMs += stallDuration)
-                        while (requestCount >= MAX_REQUESTS)
-                            _mm_pause();
+                    fileCount++;
+                    totalSize += size;
+                    maxSize = Max(size, maxSize);
                 }
                 
-                else Errf("ERROR: \"%.*s\"\n", StrExpand(path));
+                AtomicInc64(&requestCount);
+                while (requestCount >= MAX_REQUESTS)
+                    _mm_pause();
             }
+            
+            else Errf("ERROR: \"%.*s\"\n", StrExpand(path));
         }
     }
     
     while (requestCount)
         _mm_pause();
-    pushFileMs -= stallMs;
+    
     loadMs = OSNowMS() - loadMs;
-    Outf("Total Stall: %llu ms, Bandwidth: %.2f MiB/s, Loaded Files: %llu (%.2f MiB)\n",
-         stallMs, DivF64(totalSize, 1000 * loadMs), fileCount, DivF64(totalSize, MiB(1)));
+    Outf("Bandwidth: %.2f MiB/s, Loaded Files: %llu (%.2f MiB)\n",
+         DivF64(totalSize, 1000 * loadMs), fileCount, DivF64(totalSize, MiB(1)));
     return maxSize;
 }
-#endif
 
 int main(i32 argc, char** argv)
 {
@@ -656,13 +549,8 @@ int main(i32 argc, char** argv)
                         u64 num = U64FromStr(opt->values.first->string, 10, 0);
                         if (InRange(num, 1, KiB(8)))
                         {
-#if CHUNK_SIZE
                             chunkSize = KiB(num);
                             Assert(chunkSize % 4096 == 0);
-#else
-                            desiredSize = MiB(num);
-#endif
-                            
                             goto FLAG_FILE;
                         }
                     }
@@ -768,13 +656,7 @@ int main(i32 argc, char** argv)
                 if (paths.nodeCount)
                 {
                     if (flags & Glob_CLI_Debug)
-                    {
-#if CHUNK_SIZE
                         Outf("Chunk size: %llu KiB, Threads: %d\n", chunkSize/KiB(1), THREAD_COUNT);
-#else
-                        Outf("Outstanding Buffer: %.2f MiB, Threads: %d\n", DivF64(desiredSize, MiB(1)), THREAD_COUNT);
-#endif
-                    }
                     
                     u64 maxSize = ReadFilesAsync(scratch, &paths);
                     memoryUsage += maxSize;
@@ -808,7 +690,6 @@ int main(i32 argc, char** argv)
                 }
             }
             
-#if 0
             if (matchCount)
                 Outf("\n");
             Outf("Found %llu match(es) in %llu ", matchCount, paths.nodeCount);
@@ -816,27 +697,20 @@ int main(i32 argc, char** argv)
                 Outf("files (%.2f MiB)\n", DivF64(totalSize, MiB(1)));
             else
                 Outf("file names\n");
-#endif
             
             ms = OSNowMS() - ms;
             if (flags & Glob_CLI_Debug)
             {
-#if 0
                 memoryUsage += scratch->highWaterMark;
                 Outf("Total: %llums, Memory Usage: %.3f MiB, Loaded Files: %llu\n",
                      ms, DivF64(memoryUsage, MiB(1)), fileCount);
                 
                 Outf("Iter File: %llums, ", iterFileMs);
                 if (globFile)
-                    Outf("Push File: %llums, Wait File: %.2f ms/Thread, Glob File: %.2f ms/Thread\n",
-                         pushFileMs, DivF64(waitFileMs, THREAD_COUNT), DivF64(globFileMs, THREAD_COUNT));
+                    Outf("Wait File: %.2f ms/Thread, Glob File: %.2f ms/Thread\n",
+                         DivF64(waitFileMs, THREAD_COUNT), DivF64(globFileMs, THREAD_COUNT));
                 else
                     Outf("Glob: %llums\n", globFileMs);
-                
-#elif 0
-                Outf("Total: %llums, Push File: %llums, Wait File: %.2f ms/Thread\n",
-                     ms, pushFileMs, DivF64(waitFileMs, THREAD_COUNT));
-#endif
             }
         }
     }
